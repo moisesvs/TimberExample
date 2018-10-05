7d3e2fcce9ccf63bf52389e69ef294ba2544a9c8
Moisés Vázquez Sánchez <moises.vazquez.sanchez@bbva.com>
2018-10-05 10:31:05 +0200

    feat: first commit

---
+++ app/src/main/java/com/moises/timberexample/TreeExample.kt
@@ -0,0 +1,11 @@
+package com.moises.timberexample
+
+import timber.log.Timber
+
+class TreeExample : Timber.Tree() {
+    override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+
+
+
+    }
+}
